08d1c80f0c588092c91ecd50de79b7db2fbf234a
ValentinHG321 <dart.veidar1994@abv.bg>
2022-03-26 16:38:39 +0200

    adding project NewTest

---
+++ NewTest/NewTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace NewTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            BigInteger count = input;
+            
+
+            for (int i = 1; i < input; i++)
+            {
+                count *= i;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
